5d0e0b7a5ab9abd0f12f75510f2f36915ddc765a
Beyza Nur Özkan <beyzanurozkann877@gmail.com>
2021-12-13 18:39:44 +0300

    Beyza

--- SiparisOtomasyon/IncomingOrder.Designer.cs
+++ SiparisOtomasyon/IncomingOrder.Designer.cs
@@ -29,13 +29,24 @@ namespace SiparisOtomasyon
         /// </summary>
         private void InitializeComponent()
         {
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 57);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(776, 381);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // IncomingOrder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 496);
+            this.Controls.Add(this.listView1);
             this.Name = "IncomingOrder";
             this.Text = "Gelen Siparişler";
             this.ResumeLayout(false);
@@ -43,5 +54,7 @@ namespace SiparisOtomasyon
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
     }
 }
--- SiparisOtomasyon/NewOrder.Designer.cs
+++ SiparisOtomasyon/NewOrder.Designer.cs
@@ -29,13 +29,24 @@ namespace SiparisOtomasyon
         /// </summary>
         private void InitializeComponent()
         {
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(776, 426);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // NewOrder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
             this.Name = "NewOrder";
             this.Text = "Yeni Sipariş";
             this.ResumeLayout(false);
@@ -43,5 +54,7 @@ namespace SiparisOtomasyon
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
     }
 }
