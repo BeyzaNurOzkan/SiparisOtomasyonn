4031af5e06d2554b5dd5b48dcf74fffcf043c600
Beyza Nur Özkan <beyzanurozkann877@gmail.com>
2021-12-25 10:03:38 +0300

    Beyza

--- SiparisOtomasyon/Users.cs
+++ SiparisOtomasyon/Users.cs
@@ -16,8 +16,8 @@ namespace SiparisOtomasyon
         public string Name { get; set; }
         public string[,] UserAddress = new string[5, 3];
         public string UserName { get; set; }
-        public string AddressTitle { get;  set; }
-        public string Address { get;  set; }
+        public string AddressTitle { get; set; }
+        public string Address { get; set; }
         public string Password { get; set; }
         public Boolean State { get; set; }
         public List<Users> User;
